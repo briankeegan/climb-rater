b51f0f07286f8486b89eb668c1b5463cf4b2d7d7
briankeegan <brian.morris.keegan@gmail.com>
2018-03-06 11:57:12 -0500

    Update App to make call to API
    -  Add constructor with state, sections set to null
    -  create getSection method and bind to constructor
      -  Invoked getSection will fetch data from server
      and set the state with the data
    
    -  When the app component is mounted getSection is Invoked
    - When unmounted state is reset to null
    
    -Render image and name of fist section for testing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      sections: null
+    }
+    // allows getSection to always have access to the state
+    this.getSection = this.getSection.bind(this)
+  }
+
+  getSection() {
+    return fetch(`http://localhost:4741/`)
+      .then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(myJson =>  {
+        this.setState({
+          sections: myJson.sections
+        })
+        console.log(this.state)
+      })
+  }
+  componentDidMount() {
+    this.getSection()
+  }
+  componentWillUnmount() {
+    this.setState({
+      sections: null
+    })
+  }
   render() {
+    const { sections } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {sections &&
+          <div>
+            <img src={sections[0].imageURL} alt={sections[0].name} />
+            <h1>{sections[0].name}</h1>
+          </div>
+        }
       </div>
     );
   }
