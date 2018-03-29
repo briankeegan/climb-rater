c68a702f41230887763d47255cd6ffc8fad63ca8
briankeegan <brian.morris.keegan@gmail.com>
2018-03-29 19:52:54 -0400

    Comment code and re-organize apps render / routers
    -  Render now will first check if section does not exist, and if it doens't show spinner
    - Order: Section, Walls, ClimbingRoutes
    - Add not-found route, to default to if route not found
    -  Special thanks for help from Ryan Ragle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 import './App.css'
 import Nav from './Nav'
 import Spinner from './Spinner'
@@ -36,6 +36,8 @@ class App extends Component {
     })
   }
 
+  // Gathers data from api needed to display content
+  // Sets state of section so updates happen universally.
   getSection() {
     return fetch('https://climb-rater-api-development.herokuapp.com')
       .then(res => res.json())
@@ -50,16 +52,31 @@ class App extends Component {
   componentDidMount() {
     this.getSection()
   }
+  // When component is unmounted clear all data
   componentWillUnmount() {
     this.setState({
       sections: null,
-      users: null
+      user: null
     })
   }
 
   render() {
 
     const { sections, user } = this.state
+
+    // Show materialize spinner while waiting for sections data
+    if (!sections) {
+      return (
+        <div>
+          <Nav
+            user={user}
+            setUserState={this.setUserState}
+          />
+          <Spinner />
+        </div>
+      )
+    }
+
     return (
       <div>
         <Nav
@@ -67,65 +84,55 @@ class App extends Component {
           setUserState={this.setUserState}
         />
         <div className="container">
-          { sections ?
-            (
-              <Route exact path="/" render={() => <Sections sections={sections}/>}/>
-            )
-            :
-            (
-              <Spinner />
-            )
+          <Switch>
+            {/* main view displaying all climbing Sections */}
+            <Route exact path="/" render={() => <Sections sections={sections}/>} />
 
-          }
+            {/* ection view, showing single Section and all associated Walls (walls are numbered) */}
+            <Route exact path="/:name"
+              render={({ match }) => {
+                const section = sections.find(s => toUrl(s.name) === toUrl(match.params.name))
+                return section ? <Walls section={section}/> : <NotFound />
+              }}
+            />
 
-          { sections && (
-            <div>
-              <Route exact path="/:name" render={( {match} ) => {
-                const section = sections.find(s => {
-                  return toUrl(s.name) === toUrl(match.params.name)
-                })
-                return section ?
-                  (
-                    <Walls section={section}/>
-                  )
-                  :
-                  (
-                    <NotFound />
-                  )
-              }}/>
-              <Route path={'/:name/:number/:color'} render={({ match }) => {
-                const section = sections.find(s => {
-                  return toUrl(s.name) === toUrl(match.params.name)
-                })
-                const wall = section.walls.find(w => {
-                  return +w.number === +match.params.number
-                })
+            {/*
+              ClimbgingRoute view, based on name of Section, Wall number and ClimbingRoute color
+              Shows single ClimbingRoute with option for authenticated user to login
+            */}
+            <Route
+              path={'/:name/:number/:color'}
+              render={({ match }) => {
+                const section = sections.find(s => toUrl(s.name) === toUrl(match.params.name))
+                // match.params.numbers is converted to number to compare with Wall number
+                const wall = section.walls.find(w => w.number === +match.params.number)
                 let climbingRoute
+
+                // make both param and ClimbningRoute lowerCase in case of data error or user input error
                 if (wall) {
-                  climbingRoute = wall.climbingRoutes.find(cr => {
-                    return (cr.color.toLowerCase() ===    match.params.color.toLowerCase())
-                  })
+                  climbingRoute = wall.climbingRoutes.find(cr =>
+                    cr.color.toLowerCase() === match.params.color.toLowerCase())
                 }
-                return climbingRoute ?
-                  (
+
+                return (
+                  climbingRoute ?
                     <ClimbingRoute climbingRoute={climbingRoute}
                       wall={wall}
                       user={user}
                       getSection={this.getSection}
                     />
-                  )
-                  :
-                  (
+                    :
                     <NotFound />
-                  )
-              } }/>
-            </div>
-          )
-          }
+                )
+              }}
+            />
+            // Default for unknown route
+            <Route path="*" component={NotFound} />
+          </Switch>
         </div>
       </div>
     )
   }
 }
 
-export default App;
+export default App
