36fc3a196d01f0e1fa5fd376c5bcd1d61d690a12
briankeegan <brian.morris.keegan@gmail.com>
2018-03-29 23:37:46 -0400

    Comment and fix spacing of RateClimbModal & RateWithCarabiners
    Test, test and test again.
    Remove "" and replace with ''

--- src/RateClimbModal.js
+++ src/RateClimbModal.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css'
 import {Modal, Button, Input} from 'react-materialize'
 import setErrorMessageState from './SetErrorMessageState'
 import RateWithCarabiners from './RateWithCarabiners'
 
 class RateClimbModal extends Component {
-  constructor(props) {
+  constructor() {
     super()
     this.state = {
       errorMessage: '',
@@ -23,56 +23,55 @@ class RateClimbModal extends Component {
       headers: new Headers({
         'Content-Type': 'application/json',
         'Authorization': `Token token=${user.token}`
-    }),
-       method: 'DELETE'
+      }),
+      method: 'DELETE'
     })
-    .then(res => {
-      if (res.status === 204) {
-        this.setState({
-          successMessage: "Rating deleted"
-        })
-        setTimeout(() => {
+      .then(res => {
+        if (res.status === 204) {
           this.setState({
-            successMessage: ""
+            successMessage: 'Rating deleted'
           })
-          document.querySelectorAll('.modal-close').forEach(m =>
-            m.click()
-          )
-          document.querySelector('#rate_climb_form').reset()
+          setTimeout(() => {
+            this.setState({
+              successMessage: ''
+            })
+            document.querySelectorAll('.modal-close').forEach(m =>
+              m.click()
+            )
+            document.querySelector('#rate_climb_form').reset()
+          }
+            , 350)
+        } else {
+          this.setErrorMessageState('Unable to process your request.  You shouldn\'t be seeing this... sorry')
         }
-        , 350)
-      } else {
-        this.setErrorMessageState('Unable to process your request.  You shouldn\'t be seeing this... sorry')
-      }
-    })
-    .catch(error =>   this.setErrorMessageState('Unable to process your request.'))
-    .then(() => this.props.getSection())
+      })
+      .catch(() => this.setErrorMessageState('Unable to process your request.'))
+      .then(() => this.props.getSection())
 
   }
 
-  // store routeId getSection saveRating number color
-
   onSubmit (e) {
     e.preventDefault()
+    
     const carabiners = document.getElementsByName('carabiners')
-    let rating = 0;
+    let rating = 0
     for (let i = 0; i < carabiners.length; i++) {
       if (carabiners[i].checked) {
-        rating += +carabiners[i].value
+        rating = +carabiners[i].value
         break
       }
     }
     const grade = document.querySelector('.select-dropdown').value
     const user = this.props.store.user
-    const routeId = this.props.routeId
+    const { routeId, saveRating } = this.props
 
-    const saveRating = this.props.saveRating
     // if saveRatings is zero, or grade is default
+    // if theRatings has been saved, we want to update, instead of create a new rating
     const urlExt = saveRating ? `/${saveRating._id}` : ''
     const restful = saveRating ? 'PATCH' : 'POST'
-    const success = saveRating ? "Rating updated" : "Thanks for your input!"
+    const success = saveRating ? 'Rating updated' : 'Thanks for your input!'
 
-    if(!rating || (grade ==="Grade")) {
+    if(!rating || (grade ==='Grade')) {
       return this.setErrorMessageState('All fields required')
     }
 
@@ -81,35 +80,36 @@ class RateClimbModal extends Component {
       headers: new Headers({
         'Content-Type': 'application/json',
         'Authorization': `Token token=${user.token}`
-    }),
-       method: restful,
-       body: JSON.stringify({
-         "rating": {
-           "climberGrade": grade,
-           "climberRating": rating,
-           "_climbingRoute": routeId
-         }
-       })
+      }),
+      method: restful,
+      body: JSON.stringify({
+        'rating': {
+          'climberGrade': grade,
+          'climberRating': rating,
+          '_climbingRoute': routeId
+        }
+      })
     })
       .then(res => {
+        // if successful, show success message and then close the Modal
         if ((res.status === 201) || (res.status === 204)) {
           this.setState({
             successMessage: success
           })
           setTimeout(() => {
             this.setState({
-              successMessage: ""
+              successMessage: ''
             })
             document.querySelectorAll('.modal-close').forEach(m =>
               m.click()
             )
           }
-          , 350)
+            , 350)
         } else {
           this.setErrorMessageState('Unable to process your request.  You shouldn\'t be seeing this... sorry')
         }
       })
-      .catch(error =>   this.setErrorMessageState('Unable to process your request.'))
+      .catch(() =>   this.setErrorMessageState('Unable to process your request.'))
       .then(() => this.props.getSection())
 
   }
@@ -117,75 +117,74 @@ class RateClimbModal extends Component {
   render() {
     const { saveRating, number, color} = this.props
 
-  return (
-    <Modal
-    header={`${color.toUpperCase()}: #${number}`}
-    trigger={<Button waves='light'>
-      {saveRating ? 'Update Rating' : 'Rate Climb'}
-     </Button>}>
-    <div className="row">
-     <form id="rate_climb_form" className="col s12" onSubmit={this.onSubmit}>
-     <h5 className="center before-rating">How was the Route?</h5>
-      <div className="row">
-        <RateWithCarabiners saveRating={saveRating}/>
-      </div>
+    return (
+      <Modal
+        header={`${color.toUpperCase()}: #${number}`}
+        trigger={<Button waves='light'>
+          {saveRating ? 'Update Rating' : 'Rate Climb'}
+        </Button>}>
         <div className="row">
-          <Input s={5} type='select' label="What did the route feel like?" defaultValue={
-            (saveRating && saveRating.climberGrade) || 0
-          }>
-            <option value='0' disabled>Grade</option>
-            <option value='5.5'>5.5</option>
-            <option value='5.6'>5.6</option>
-            <option value='5.7'>5.7</option>
-            <option value='5.8'>5.8</option>
-            <option value='5.9'>5.9</option>
-            <option value='5.10a'>5.10a</option>
-            <option value='5.10b'>5.10b</option>
-            <option value='5.10c'>5.10c</option>
-            <option value='5.10d'>5.10d</option>
-            <option value='5.11a'>5.11a</option>
-            <option value='5.11b'>5.11b</option>
-            <option value='5.11c'>5.11c</option>
-            <option value='5.11d'>5.11d</option>
-            <option value='5.12a'>5.12a</option>
-            <option value='5.12b'>5.12b</option>
-            <option value='5.12c'>5.12c</option>
-            <option value='5.12d'>5.12d</option>
-            <option value='5.13a'>5.13a</option>
-            <option value='5.13b'>5.13b</option>
-            <option value='5.13c'>5.13c</option>
-            <option value='5.13d'>5.13d</option>
-            <option value='5.14a'>5.14a</option>
-            <option value='5.14b'>5.14b</option>
-            <option value='5.14c'>5.14c</option>
-            <option value='5.14d'>5.14d</option>
-            <option value='5.15'>5.15</option>
+          <form id="rate_climb_form" className="col s12" onSubmit={this.onSubmit}>
+            <h5 className="center before-rating">How was the Route?</h5>
+            <div className="row">
+              <RateWithCarabiners saveRating={saveRating}/>
+            </div>
+            <div className="row">
+              <Input s={5} type='select' label="What did the route feel like?" defaultValue={
+                (saveRating && saveRating.climberGrade) || 0
+              }>
+                <option value='0' disabled>Grade</option>
+                <option value='5.5'>5.5</option>
+                <option value='5.6'>5.6</option>
+                <option value='5.7'>5.7</option>
+                <option value='5.8'>5.8</option>
+                <option value='5.9'>5.9</option>
+                <option value='5.10a'>5.10a</option>
+                <option value='5.10b'>5.10b</option>
+                <option value='5.10c'>5.10c</option>
+                <option value='5.10d'>5.10d</option>
+                <option value='5.11a'>5.11a</option>
+                <option value='5.11b'>5.11b</option>
+                <option value='5.11c'>5.11c</option>
+                <option value='5.11d'>5.11d</option>
+                <option value='5.12a'>5.12a</option>
+                <option value='5.12b'>5.12b</option>
+                <option value='5.12c'>5.12c</option>
+                <option value='5.12d'>5.12d</option>
+                <option value='5.13a'>5.13a</option>
+                <option value='5.13b'>5.13b</option>
+                <option value='5.13c'>5.13c</option>
+                <option value='5.13d'>5.13d</option>
+                <option value='5.14a'>5.14a</option>
+                <option value='5.14b'>5.14b</option>
+                <option value='5.14c'>5.14c</option>
+                <option value='5.14d'>5.14d</option>
+                <option value='5.15'>5.15</option>
+              </Input>
+            </div>
 
-          </Input>
+            <div className="center">
+              <h5 className="red-text">{this.state.errorMessage}</h5>
+            </div>
+            <div className="center">
+              <h5 className="green-text">{this.state.successMessage}</h5>
+            </div>
+            <Button waves='light'>
+              {saveRating ? 'Update!' : 'Submit!'}
+            </Button>
+
+            {saveRating &&
+               (
+                 <Button type="button" className="red right" waves='light' onClick={this.onDelete}>
+                   Delete Rating
+                 </Button>
+               )
+            }
+          </form>
         </div>
 
-        <div className="center">
-          <h5 className="red-text">{this.state.errorMessage}</h5>
-        </div>
-        <div className="center">
-          <h5 className="green-text">{this.state.successMessage}</h5>
-        </div>
-        <Button waves='light'>
-          {saveRating ? 'Update!' : 'Submit!'}
-         </Button>
-
-         {saveRating &&
-           (
-             <Button type="button" className="red right" waves='light' onClick={this.onDelete}>
-               Delete Rating
-              </Button>
-           )
-         }
-      </form>
- </div>
-
-  </Modal>
-)}
+      </Modal>
+    )}
 }
 
-export default RateClimbModal;
+export default RateClimbModal
--- src/RateWithCarabiners.js
+++ src/RateWithCarabiners.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import carabiner from './carabiner.png';
+import React, { Component } from 'react'
+import carabiner from './carabiner.png'
 
 class RateWithCarabiners extends Component {
   constructor(props) {
@@ -15,34 +15,34 @@ class RateWithCarabiners extends Component {
   }
 
   render () {
-  return (
-    <div className="carabiners-container">
+    return (
+      <div className="carabiners-container">
         <input name="carabiners" value="5" type="radio" id="rate5"  checked={this.state.selectedOption === '5'}
-                      onChange={this.handleOptionChange}  />
-      <label htmlFor="rate5"><img src={carabiner} alt="carabiner-icon"/></label>
+          onChange={this.handleOptionChange}  />
+        <label htmlFor="rate5"><img src={carabiner} alt="carabiner-icon"/></label>
 
-      <input name="carabiners" value="4" type="radio" id="rate4"
-      checked={this.state.selectedOption === '4'}
-                      onChange={this.handleOptionChange} />
-      <label htmlFor="rate4"><img src={carabiner} alt="carabiner-icon"/></label>
+        <input name="carabiners" value="4" type="radio" id="rate4"
+          checked={this.state.selectedOption === '4'}
+          onChange={this.handleOptionChange} />
+        <label htmlFor="rate4"><img src={carabiner} alt="carabiner-icon"/></label>
 
-      <input name="carabiners" value="3" type="radio" id="rate3"
-      checked={this.state.selectedOption === '3'}
-                      onChange={this.handleOptionChange} />
-      <label htmlFor="rate3"><img src={carabiner} alt="carabiner-icon"/></label>
+        <input name="carabiners" value="3" type="radio" id="rate3"
+          checked={this.state.selectedOption === '3'}
+          onChange={this.handleOptionChange} />
+        <label htmlFor="rate3"><img src={carabiner} alt="carabiner-icon"/></label>
 
-      <input name="carabiners" value="2" type="radio" id="rate2"
-      checked={this.state.selectedOption === '2'}
-                      onChange={this.handleOptionChange} />
-      <label htmlFor="rate2"><img src={carabiner} alt="carabiner-icon"/></label>
+        <input name="carabiners" value="2" type="radio" id="rate2"
+          checked={this.state.selectedOption === '2'}
+          onChange={this.handleOptionChange} />
+        <label htmlFor="rate2"><img src={carabiner} alt="carabiner-icon"/></label>
 
-      <input name="carabiners" value="1" type="radio" id="rate1"
-      checked={this.state.selectedOption === '1'}
-                      onChange={this.handleOptionChange} />
-      <label htmlFor="rate1"><img src={carabiner} alt="carabiner-icon"/></label>
+        <input name="carabiners" value="1" type="radio" id="rate1"
+          checked={this.state.selectedOption === '1'}
+          onChange={this.handleOptionChange} />
+        <label htmlFor="rate1"><img src={carabiner} alt="carabiner-icon"/></label>
 
-    </div>
-  )}
+      </div>
+    )}
 }
 
-export default RateWithCarabiners;
+export default RateWithCarabiners
