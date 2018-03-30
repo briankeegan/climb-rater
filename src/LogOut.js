94bb1f7626daa05c0af2eb3b17562add3e46259a
briankeegan <brian.morris.keegan@gmail.com>
2018-03-29 20:28:50 -0400

    Refactor Logout and SignUpModal
    Clean up code and test that everything is still working

--- src/LogOut.js
+++ src/LogOut.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const logOut = function(data, setUserState) {
   const user = data.user
-   fetch(`https://climb-rater-api-development.herokuapp.com/sign-out/${user.id}`, {
+  fetch(`https://climb-rater-api-development.herokuapp.com/sign-out/${user.id}`, {
     headers: new Headers({
-    'Content-Type': 'application/json',
-    'Authorization': `Token token=${user.token}`
-  }),
-     method: 'DELETE'
+      'Content-Type': 'application/json',
+      'Authorization': `Token token=${user.token}`
+    }),
+    method: 'DELETE'
   })
     .then(() =>  {
       setUserState(null)
     })
-    .catch(error =>
+    .catch(() =>
       setUserState(null)
     )
 }
@@ -21,4 +21,4 @@ const LogOut = ({ user, setUserState }) => (
   <a onClick={() => logOut(user, setUserState)}>Log Out</a>
 )
 
-export default LogOut;
+export default LogOut
--- src/SignUpModal.js
+++ src/SignUpModal.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css'
 import {Modal, Button} from 'react-materialize'
 import setErrorMessageState from './SetErrorMessageState'
 
 class SignUpModal extends Component {
-  constructor(props) {
+  constructor() {
     super()
     this.state = {
       errorMessage: '',
@@ -15,87 +15,87 @@ class SignUpModal extends Component {
   }
 
 
-  onSubmit (e, setUserState) {
+  onSubmit (e) {
     e.preventDefault()
     const user_name = document.getElementById('sign_up_user_name').value
     const password = document.getElementById('sign_up_password').value
     const password_retype = document.getElementById('sign_up_password_retype').value
 
-  // Assuring that form is filled out!
-  if ([user_name, password, password_retype].some(cur => cur === "")) {
-    return this.setErrorMessageState('No blank fields allowed.')
-  }
+    // Assuring that form is filled out!
+    if ([user_name, password, password_retype].some(cur => cur === '')) {
+      return this.setErrorMessageState('No blank fields allowed.')
+    }
     // checking for matching passwords
     if (password !== password_retype) {
       return this.setErrorMessageState('Passwords don\'t match.')
     }
-    // send http reqeust
-    fetch(`https://climb-rater-api-development.herokuapp.com/sign-up`, {
+    // send http reqeust, regardless of success or failure reset data (incase offline)
+    fetch('https://climb-rater-api-development.herokuapp.com/sign-up', {
       headers: new Headers({
-      'Content-Type': 'application/json'
-    }),
-       method: 'POST',
-       body: JSON.stringify({
-         "credentials": {
-           "email": user_name,
-           "password": password
-         }
-       })
+        'Content-Type': 'application/json'
+      }),
+      method: 'POST',
+      body: JSON.stringify({
+        'credentials': {
+          'email': user_name,
+          'password': password
+        }
+      })
     })
-    .then(res => res.json())
-    .then(myJson =>  {
-      if (myJson.error)
-      return this.setErrorMessageState('Unable to process your request.  Please try again!')
-      document.querySelectorAll('.modal-close').forEach(m =>
-        m.click()
-      )
-      document.querySelector('#log_in_form').reset()
-      return myJson
-    })
-    .then(this.props.setUserState)
-    .catch(error => error)
+      .then(res => res.json())
+      .then(myJson =>  {
+        if (myJson.error)
+          return this.setErrorMessageState('Unable to process your request.  Please try again!')
+        document.querySelectorAll('.modal-close').forEach(m =>
+          m.click()
+        )
+        document.querySelector('#log_in_form').reset()
+        return myJson
+      })
+      .then(this.props.setUserState)
+      .catch(error => error)
   }
 
   render() {
-  return (
-    <Modal
-    header='Sign Up'
-    trigger={<a href="#portfolio">Sign Up</a>}>
-    <div className="row">
-   <form id="sign_up_form" className="col s12" onSubmit={this.onSubmit}>
+    return (
+      <Modal
+        header='Sign Up'
+        trigger={<a href="#portfolio">Sign Up</a>}>
+        <div className="row">
+          <form id="sign_up_form" className="col s12" onSubmit={this.onSubmit}>
 
-     <div className="row">
-       <div className="input-field col m8 offset-m2">
-         <input id="sign_up_user_name" type="text" className="validate" required="true" />
-         <label htmlFor="sign_up_user_name">User Name</label>
-       </div>
-     </div>
-     <div className="row">
-       <div className="input-field col m8 offset-m2">
-         <input id="sign_up_password" type="password" className="validate" required="true" />
-         <label htmlFor="sign_up_password">Password</label>
-       </div>
-     </div>
-     <div className="row">
-       <div className="input-field col m8 offset-m2">
-         <input id="sign_up_password_retype" type="password" className="validate" required="true"/>
-         <label htmlFor="sign_up_password_retype">Retype Password</label>
-       </div>
-     </div>
-      <div className="center">
-        <h5 className="red-text">{this.state.errorMessage}</h5>
-      </div>
-      <div className="center">
-        <h5 className="green-text">{this.state.successMessage}</h5>
-      </div>
-      <Button waves='light'>
-        Submit!
-       </Button>
-   </form>
- </div>
+            <div className="row">
+              <div className="input-field col m8 offset-m2">
+                <input id="sign_up_user_name" type="text" className="validate" required="true" />
+                <label htmlFor="sign_up_user_name">User Name</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col m8 offset-m2">
+                <input id="sign_up_password" type="password" className="validate" required="true" />
+                <label htmlFor="sign_up_password">Password</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col m8 offset-m2">
+                <input id="sign_up_password_retype" type="password" className="validate" required="true"/>
+                <label htmlFor="sign_up_password_retype">Retype Password</label>
+              </div>
+            </div>
+            <div className="center">
+              <h5 className="red-text">{this.state.errorMessage}</h5>
+            </div>
+            <div className="center">
+              <h5 className="green-text">{this.state.successMessage}</h5>
+            </div>
+            <Button waves='light'>
+              Submit!
+            </Button>
+          </form>
+        </div>
 
-  </Modal>
-)}
+      </Modal>
+    )}
 }
 
-export default SignUpModal;
+export default SignUpModal
